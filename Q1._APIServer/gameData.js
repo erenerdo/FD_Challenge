d25827760f8974d93b212dd3c0507f01b83a05ab
Eren Erdogan <ererdogan20@gmail.com>
2018-03-30 20:44:40 -0400

    finished players section

--- Q1._APIServer/gameData.js
+++ Q1._APIServer/gameData.js
@@ -1,6 +1,7 @@
-const games = [{id: 1, home_team_id: 1, away_team_id: 2, date: "1/1/2016"},
-{id: 2, home_team_id: 3, away_team_id: 4, date: "1/1/2016"},
-{id: 3, home_team_id: 5, away_team_id: 6, date: "1/1/2016"},
-{id: 4, home_team_id: 2, away_team_id: 3, date: "1/2/2016"},
-{id: 5, home_team_id: 1, away_team_id: 4, date: "1/3/2016"}];
+const games = [{id: 1, home_team_id: 1, away_team_id: 2, date: "01012016"},
+{id: 2, home_team_id: 3, away_team_id: 4, date: "01012016"},
+{id: 3, home_team_id: 5, away_team_id: 6, date: "01012016"},
+{id: 4, home_team_id: 2, away_team_id: 3, date: "01022016"},
+{id: 5, home_team_id: 1, away_team_id: 4, date: "01032016"}];
 
+module.exports = games;
--- Q1._APIServer/routes/players.js
+++ Q1._APIServer/routes/players.js
@@ -1,4 +1,72 @@
 const express = require('express');
 const router = express.Router();
+const playerData = require('../playerData');
+const playerStats = require('../playerStats');
+const gameData = require('../gameData');
+
+router.get('/', (req, res, next) => {
+  const date = req.query.date;
+  if (date === undefined) res.json(playerData);
+  else res.json(getPlayersFromDate(date));
+});
+
+router.get('/:id/stats', (req, res, next) => {
+  const playerId = +req.params.id;
+  const stats = [];
+
+  for (let stat of playerStats) {
+    if (stat.player_id === playerId) {
+      stats.push(stat);
+    }
+  }
+  res.json(stats);
+});
+
+router.get('/:id', (req, res, next) => {
+  const playerId = req.params.id;
+
+  res.json(playerData[playerId - 1]);
+});
+
+
+function getPlayersFromDate (date) {
+  const games = getGamesFromDate(date);
+  const teams = getTeamsFromGames(games);
+  return getPlayersFromTeamIds(teams);
+}
+
+function getPlayersFromTeamIds (teams) {
+  const teamIdSet = new Set(teams);
+  const players = [];
+
+  for (let player of playerData) {
+    if (teamIdSet.has(player.team_id)) {
+      players.push(player);
+    }
+  }
+  return players;
+}
+
+function getGamesFromDate (date) {
+  const games = [];
+
+  for (let game of gameData) {
+    if (game.date === date) {
+      games.push(game);
+    }
+  }
+  return games;
+}
+
+function getTeamsFromGames(games) {
+  let teams = [];
+
+  for (let game of games) {
+    teams.push(game.home_team_id);
+    teams.push(game.away_team_id);
+  }
+
+  return teams;
+}
 
 module.exports = router;

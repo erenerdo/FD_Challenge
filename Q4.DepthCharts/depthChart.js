5c68e4a7ec96e80ff67216f6ca3bac4701290e4e
Eren Erdogan <ererdogan20@gmail.com>
2018-04-01 15:22:29 -0400

    finished NFL tests

--- Q4.DepthCharts/depthChart.js
+++ Q4.DepthCharts/depthChart.js
@@ -1,3 +1,9 @@
+/*
+
+Created a depth chart class to be used for any sport. The depth chart class takes in an array of position for it's constructor and will create the depth chart based off the positions entered in
+
+*/
+
 class DepthChart {
   constructor(positions) {
     // Create depth chart map
@@ -19,7 +25,7 @@ class DepthChart {
       let curPlayer = positionPlayers[i];
 
       // If the player we are trying to add is in the depth chart, remove them
-      if (curPlayer.id === player.id) {
+      if (curPlayer.player_id === player.player_id) {
         this.removePlayerAtIdx(positionPlayers, i);
         break;
       }
@@ -36,7 +42,7 @@ class DepthChart {
       let curPlayer = positionPlayers[i];
 
       // If the curPlayer is the player we want to remove, remove them
-      if (curPlayer.id === player.id) {
+      if (curPlayer.player_id === player.player_id) {
         this.removePlayerAtIdx(positionPlayers, i);
         break;
       }
@@ -44,11 +50,30 @@ class DepthChart {
   }
 
   getFullDepthChart() {
-    console.log(this.depthChartMap);
+    console.log('\nCurrent Depth Chart:');
+
+    for (let position in this.depthChartMap) {
+      if (this.depthChartMap[position].length > 0) {
+        let IdsForPosition = this.depthChartMap[position].map(player => player.player_id);
+        console.log(position + ':', IdsForPosition);
+      }
+    }
   }
 
   gertPlayersUnderPlayerInDepthChart(player, position) {
-    return this.depthChartMap[position];
+    console.log('\nPlayers under', player.name, 'for', position + ':');
+
+    let positionPlayers = this.depthChartMap[position];
+
+    for (let i = 0; i < positionPlayers.length; i++) {
+      let curPlayer = positionPlayers[i];
+
+      // If the curPlayer is the player we're looking for output array of players under them
+      if (curPlayer.player_id === player.player_id) {
+        console.log(positionPlayers.slice(i + 1).map(p => p.player_id));
+        break;
+      }
+    }
   }
 
   removePlayerAtIdx(positionPlayers, idx) {
@@ -68,9 +93,9 @@ var lebron = {
   name: 'King James'
 };
 
-var kristaps = {
+var melo = {
   player_id: 2,
-  name: 'Kristaps'
+  name: 'Carmelo'
 };
 
 var odell = {
@@ -78,9 +103,9 @@ var odell = {
   name: 'OBJ'
 };
 
-var melvin = {
+var antonio = {
   player_id: 4,
-  name: 'Melvin Gordon'
+  name: 'Antonio Brown'
 };
 
 var aaron = {
@@ -90,4 +115,27 @@ var aaron = {
 
 // Tests for NFL
 
+// Create NFL depths chart from class
 const nflDepthChart = new DepthChart(['QB', 'WR', 'RB', 'TE', 'K', 'P', 'KR', 'PR']);
+
+
+// Test addPlayersToDepthChart
+nflDepthChart.addPlayerToDepthChart(lebron, 'WR', 0);
+nflDepthChart.addPlayerToDepthChart(melo, 'WR', 0);
+nflDepthChart.addPlayerToDepthChart(odell, 'WR', 2);
+nflDepthChart.addPlayerToDepthChart(antonio, 'KR', 0);
+nflDepthChart.addPlayerToDepthChart(aaron, 'QB', 0);
+nflDepthChart.addPlayerToDepthChart(lebron, 'K', 0);
+
+// Test addFullDepthChart
+nflDepthChart.getFullDepthChart();
+
+// Test getPlayersUnderPlayerInDepthChart
+nflDepthChart.gertPlayersUnderPlayerInDepthChart(melo, 'WR');
+
+// Test removePlayerFromDepthChart
+// Remove lebron from WR to test remove function
+nflDepthChart.removePlayerFromDepthChart(lebron, 'WR');
+
+// Get full depth to see if LeBron is removed from WR, Lebron is id 1
+nflDepthChart.getFullDepthChart();
